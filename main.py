8545bb305c88006f80826c1b413fd6054c8e0d49
Alencruser <alencruserxx@hotmail.fr>
2020-07-29 01:16:51 +0200

    commande help

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from keyP import token
 
 slave = commands.Bot(command_prefix=':',case_insensitive='True')
 
+slave.remove_command('help')
+
 @slave.event
 async def on_ready():
+    await slave.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=" :help"))
     print('Ready to use')
 
 @slave.command()
@@ -20,5 +23,11 @@ async def clean(ctx,num = 1):
     await ctx.message.delete()
     async for message in ctx.channel.history(limit=num):
         await message.delete()
+
+
+@slave.command()
+async def help(ctx):
+    await ctx.message.delete()
+    await ctx.send('**``` :pubg  -Vous montre une image des stats d\'arme de pubg \n :clean [nombre] -Supprime un nombre défini de messages dans le channel où cette commande est faite```**')
 
 slave.run(token)
