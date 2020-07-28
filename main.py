efd3c1a7315c624933a155b616ffb1c57e8a3fd5
Alencruser <alencruserxx@hotmail.fr>
2020-07-28 23:37:28 +0200

    fonction delete message, parametre int

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ from keyP import token
 
 slave = commands.Bot(command_prefix=':',case_insensitive='True')
 
+@slave.event
+async def on_ready():
+    print('Ready to use')
+
 @slave.command()
 async def Hello(ctx):
     await ctx.send('Hi')
 @slave.command()
 async def pubg(ctx):
+    await ctx.message.delete()
     await ctx.send('https://i.redd.it/wwmw37gr8gt01.png')
-
+@slave.command()
+async def clean(ctx,num = 1):
+    await ctx.message.delete()
+    async for message in ctx.channel.history(limit=num):
+        await message.delete()
 
 slave.run(token)
