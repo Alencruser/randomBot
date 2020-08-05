593b253e4e2d1e3fe7a69c2ba056a3c89e288c49
Alencruser <alencruserxx@hotmail.fr>
2020-08-05 16:58:15 +0200

    ajout play musique et système de playlist

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import discord
+import youtube_dl
 from discord.ext import commands
 from keyP import token
 
 slave = commands.Bot(command_prefix=':',case_insensitive='True')
-
+ytdl = youtube_dl.YoutubeDL()
 slave.remove_command('help')
+
+musics = {}
+
 
 @slave.event
 async def on_ready():
     await slave.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=" :help"))
     print('Ready to use')
+
+class Video:
+    def __init__(self, link):
+        video = ytdl.extract_info(link, download =False)
+        video_format = video["formats"][0]
+        self.url = video["webpage_url"]
+        self.stream_url = video_format["url"]
+
+def play_song(client, queue, song):
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.stream_url, before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
+
+    def next(_):
+        if len(queue) > 0:
+            new_song = queue[0]
+            del queue[0]
+            play_song(client, queue, new_song)
+
+    client.play(source,after=next)
 
 @slave.command()
 async def Hello(ctx):
@@ -31,7 +53,33 @@ async def join(ctx):
     await channel.connect()
 
 @slave.command()
+async def play(ctx, url):
+    await ctx.message.delete()
+
+    server = ctx.guild.voice_client
+
+    if server and server.channel:
+        # ici append la musique si il joue ou jouer une musique si il est co
+        video = Video(url)
+        if len(musics[ctx.guild])>0:
+            musics[ctx.guild].append(video)
+        else:
+            play_song(server, musics[ctx.guild], video)
+    else:
+        channel = ctx.author.voice.channel
+        video = Video(url)
+        musics[ctx.guild] = []
+        connected = False
+        server = await channel.connect()
+        play_song(server, musics[ctx.guild], video)
+
+@slave.command()
 async def disconnect(ctx):
+    await ctx.message.delete()
+    await ctx.voice_client.disconnect()
+
+@slave.command()
+async def leave(ctx):
     await ctx.message.delete()
     await ctx.voice_client.disconnect()
 
